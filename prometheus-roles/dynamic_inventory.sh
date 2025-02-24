53ade68dc0f1ec3a04d708cfb6b34822fc2a37ee
prince98batra <prince98batra@gmail.com>
2025-02-24 15:35:46 +0530

    Update dynamic_inventory.sh

--- prometheus-roles/dynamic_inventory.sh
+++ prometheus-roles/dynamic_inventory.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Initialize Terraform providers (Fixes the plugin issue)
+terraform -chdir=../prometheus-terraform/modules/instances init -upgrade -reconfigure
+
 # Fetch outputs from Terraform
 PUBLIC_IP=$(terraform -chdir=../prometheus-terraform/modules/instances output -raw public_instance_ip)
 PRIVATE_IP=$(terraform -chdir=../prometheus-terraform/modules/instances output -raw private_instance_ip)
 
 # Generate Ansible inventory file
-cat <<EOF
+cat <<EOL > inventory.ini
 [public]
-$PUBLIC_IP ansible_user=ubuntu
+$PUBLIC_IP ansible_user=ubuntu ansible_ssh_private_key_file=$SSH_KEY
 
 [private]
-$PRIVATE_IP ansible_user=ubuntu
-EOF
+$PRIVATE_IP ansible_user=ubuntu ansible_ssh_private_key_file=$SSH_KEY ansible_ssh_common_args='-o ProxyCommand="ssh -i $SSH_KEY -W %h:%p ubuntu@$PUBLIC_IP"'
+EOL
