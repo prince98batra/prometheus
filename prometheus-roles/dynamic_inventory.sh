59c9e7aaefef8b9dccafd93294bf62d668bfb6bd
prince98batra <prince98batra@gmail.com>
2025-02-24 16:40:59 +0530

    Update dynamic_inventory.sh

--- prometheus-roles/dynamic_inventory.sh
+++ prometheus-roles/dynamic_inventory.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Fetch Terraform outputs for public and private instance IPs
-PUBLIC_IP=$(terraform -chdir=../prometheus-terraform output -raw public_instance_ip)
-PRIVATE_IP=$(terraform -chdir=../prometheus-terraform output -raw private_instance_ip)
+# Fetch IPs from Terraform output
+public_ip=$(terraform -chdir=../prometheus-terraform output -raw public_instance_ip)
+private_ip=$(terraform -chdir=../prometheus-terraform output -raw private_instance_ip)
 
-# Generate Ansible inventory file
+# Generate Ansible dynamic inventory
 cat <<EOF > inventory.ini
 [public]
-$PUBLIC_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/mykey.pem
+$public_ip ansible_user=ubuntu ansible_ssh_private_key_file=$SSH_KEY
 
 [private]
-$PRIVATE_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/mykey.pem ansible_ssh_common_args='-o ProxyCommand="ssh -i ~/.ssh/mykey.pem -W %h:%p ubuntu@$PUBLIC_IP"'
+$private_ip ansible_user=ubuntu ansible_ssh_private_key_file=$SSH_KEY ansible_ssh_common_args='-o ProxyCommand="ssh -i $SSH_KEY -W %h:%p ubuntu@$public_ip"'
 EOF
