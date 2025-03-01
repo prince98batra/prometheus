5729c827e395fea40f6a6f833a0f98b08c93c4f4
prince98batra <prince98batra@gmail.com>
2025-03-02 00:34:03 +0530

    Update main.tf

--- prometheus-terraform/modules/security/main.tf
+++ prometheus-terraform/modules/security/main.tf
@@ -1,47 +1,59 @@
 resource "aws_security_group" "public_sg" {
   vpc_id = var.vpc_id
+
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # SSH Allowed
   }
+
   ingress {
-    from_port = 9090
-    to_port = 9090
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Prometheus Web UI Allowed
   }
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow All Outgoing Traffic
   }
-  tags = { Name = "public-sg" }
+
+  tags = {
+    Name = "public-sg"
+  }
 }
 
 resource "aws_security_group" "private_sg" {
   vpc_id = var.vpc_id
+
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    security_groups = [aws_security_group.public_sg.id]
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id] # Allow SSH from public_sg
   }
+
   ingress {
-    from_port = 9093
-    to_port = 9093
-    protocol = "tcp"
-    security_groups = [aws_security_group.public_sg.id]
+    from_port       = 9093
+    to_port         = 9093
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id] # Allow Alertmanager from public_sg
   }
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow All Outgoing Traffic
   }
-  tags = { Name = "private-sg" }
+
+  tags = {
+    Name = "private-sg"
+  }
 }
 
 output "public_sg_id" {
