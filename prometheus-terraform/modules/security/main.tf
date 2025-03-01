3166fe987e4ba3f8a41c113738bee910c5be111d
prince98batra <prince98batra@gmail.com>
2025-03-02 00:38:31 +0530

    Update main.tf

--- prometheus-terraform/modules/security/main.tf
+++ prometheus-terraform/modules/security/main.tf
@@ -5,21 +5,28 @@ resource "aws_security_group" "public_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH Allowed
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Prometheus Web UI Allowed
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9093
+    to_port     = 9093
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow All Outgoing Traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -34,21 +41,28 @@ resource "aws_security_group" "private_sg" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.public_sg.id] # Allow SSH from public_sg
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  ingress {
+    from_port       = 9090
+    to_port         = 9090
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
   }
 
   ingress {
     from_port       = 9093
     to_port         = 9093
     protocol        = "tcp"
-    security_groups = [aws_security_group.public_sg.id] # Allow Alertmanager from public_sg
+    security_groups = [aws_security_group.public_sg.id]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow All Outgoing Traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
