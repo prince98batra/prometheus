51662ab6320ca22d7607b95aba507ecc397c23c5
prince98batra <prince98batra@gmail.com>
2025-03-09 23:12:22 +0530

    Update main.tf

--- prometheus-terraform/modules/instances/main.tf
+++ prometheus-terraform/modules/instances/main.tf
@@ -1,14 +1,20 @@
 resource "aws_instance" "public_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.public_subnet_id
-  key_name      = var.key_name
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_id
+  key_name               = var.key_name
   vpc_security_group_ids = [var.public_sg_id]
-  tags = { Name = "prometheus-server" }
-}
 
+  tags = {
+    Name        = "prometheus-server"
+    fetch_name  = "prometheus-instance"
+  }
+}
 
 output "public_instance_ip" {
   value = aws_instance.public_instance.public_ip
 }
 
+output "fetch_name" {
+  value = aws_instance.public_instance.tags["fetch_name"]
+}
