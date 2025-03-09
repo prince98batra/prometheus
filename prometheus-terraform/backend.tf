1d677425d18a254e7292103e2af275a623f5a65f
prince98batra <prince98batra@gmail.com>
2025-03-10 03:16:39 +0530

    Update backend.tf

--- prometheus-terraform/backend.tf
+++ prometheus-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "prince-batra-bucket"  
+    bucket = "prince-batra-bucket-2"  
     key    = "prometheus/terraform.tfstate" 
     region = "us-east-1"                    
     encrypt = true                        
