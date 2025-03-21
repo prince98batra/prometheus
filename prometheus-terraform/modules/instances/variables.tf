c8c3e4d6ecb33b263bf61314a4a7aab98f745f9d
prince98batra <prince98batra@gmail.com>
2025-03-21 16:04:44 +0530

    Update variables.tf

--- prometheus-terraform/modules/instances/variables.tf
+++ prometheus-terraform/modules/instances/variables.tf
@@ -1,5 +1,9 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "key_name" {}
-variable "public_subnet_id" {}
-variable "public_sg_id" {}
+
+variable "public_subnet_id_1" {}
+variable "public_subnet_id_2" {}
+
+variable "public_sg_id_1" {}
+variable "public_sg_id_2" {}
