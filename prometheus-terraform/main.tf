8e910f0b0bfb31b5ae337e687967bae6d9e036f9
prince98batra <prince98batra@gmail.com>
2025-03-21 15:45:38 +0530

    Update main.tf

--- prometheus-terraform/main.tf
+++ prometheus-terraform/main.tf
@@ -18,13 +18,24 @@ module "instances" {
   ami_id = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
   key_name = "mykey"
-  public_subnet_id  = module.networking.public_subnet_ids[0]
-  public_sg_id  = module.security.public_sg_id
+  public_subnet_id_1  = module.networking.public_subnet_id_1
+  public_subnet_id_2  = module.networking.public_subnet_id_2
+  public_sg_id_1  = module.security.public_sg_id_1
+  public_sg_id_2  = module.security.public_sg_id_2
 }
 
-output "public_instance_ip" {
-  value = module.instances.public_instance_ip
+output "public_instance_ip_1" {
+  value = module.instances.public_instance_ip_1
 }
-output "fetch_name" {
-  value = module.instances.fetch_name
+
+output "public_instance_ip_2" {
+  value = module.instances.public_instance_ip_2
+}
+
+output "fetch_name_1" {
+  value = module.instances.fetch_name_1
+}
+
+output "fetch_name_2" {
+  value = module.instances.fetch_name_2
 }
