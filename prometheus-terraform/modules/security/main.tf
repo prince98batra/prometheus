9517f39d9ec86f433fb2a0659aea7ae4ed60f40a
prince98batra <prince98batra@gmail.com>
2025-03-21 15:44:41 +0530

    Update main.tf

--- prometheus-terraform/modules/security/main.tf
+++ prometheus-terraform/modules/security/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "public_sg" {
+resource "aws_security_group" "public_sg_1" {
   vpc_id = var.vpc_id
 
   ingress {
@@ -37,12 +37,58 @@ ingress {
   }
 
   tags = {
-    Name = "prometheus-sg"
+    Name = "prometheus-sg-1"
   }
 }
 
+resource "aws_security_group" "public_sg_2" {
+  vpc_id = var.vpc_id
 
-output "public_sg_id" {
-  value = aws_security_group.public_sg.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9093
+    to_port     = 9093
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "prometheus-sg-2"
+  }
+}
+
+output "public_sg_id_1" {
+  value = aws_security_group.public_sg_1.id
+}
+
+output "public_sg_id_2" {
+  value = aws_security_group.public_sg_2.id
 }
 
