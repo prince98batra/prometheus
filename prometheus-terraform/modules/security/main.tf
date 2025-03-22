bf1293147c0b8b7a11ada147f3a7989883f83b70
prince98batra <prince98batra@gmail.com>
2025-03-22 15:27:09 +0530

    Update main.tf

--- prometheus-terraform/modules/security/main.tf
+++ prometheus-terraform/modules/security/main.tf
@@ -12,21 +12,21 @@ resource "aws_security_group" "public_sg_1" {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.52/32"]
+    cidr_blocks = ["202.142.122.108/32"]
   }
 
 ingress {
     from_port   = 9100
     to_port     = 9100
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.52/32"]
+    cidr_blocks = ["202.142.122.108/32"]
   }
 
   ingress {
     from_port   = 9093
     to_port     = 9093
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.52/32"]
+    cidr_blocks = ["202.142.122.108/32"]
   }
 
   egress {
@@ -55,21 +55,21 @@ resource "aws_security_group" "public_sg_2" {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.52/32"]
+    cidr_blocks = ["202.142.122.108/32"]
   }
 
   ingress {
     from_port   = 9100
     to_port     = 9100
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.52/32"]
+    cidr_blocks = ["202.142.122.108/32"]
   }
 
   ingress {
     from_port   = 9093
     to_port     = 9093
     protocol    = "tcp"
-    cidr_blocks = ["192.168.1.52/32"]
+    cidr_blocks = ["202.142.122.108/32"]
   }
 
   egress {
